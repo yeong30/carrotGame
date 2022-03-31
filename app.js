1b635edf1b93cbbfeff0cf1eb37a82595fa34cca
uuyeong <right00730@gmail.com>
2022-03-31 09:39:34 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".game__container");
+const canvas = document.querySelector("#game__canvas");
+// const height = container.clientHeight;
+// const width = container.clientWidth;
+
+// canvas.height = height;
+// canvas.width = width;
+
+// const ctx = canvas.getContext("2d");
+
+// const backgroundImg = new Image();
+// backgroundImg.src = "assets/field.png";
+// backgroundImg.onload = () => ctx.drawImage(backgroundImg, 0, 0, width, height);
